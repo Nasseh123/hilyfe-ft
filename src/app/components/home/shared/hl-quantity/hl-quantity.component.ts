dc801ae7d4f665ac64a8cb7cc1d16e989e22d006
Langat <davidsonlangat2021@gmail.com>
2025-02-19 18:49:59 +0300

    finalysing the cart pages and add ons

---
+++ src/app/components/home/shared/hl-quantity/hl-quantity.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-hl-quantity',
+  templateUrl: './hl-quantity.component.html',
+  styleUrls: ['./hl-quantity.component.scss'],
+  standalone:true,
+  imports:[]
+})
+export class HlQuantityComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/components/home/shared/hl-quick-selection/hl-quick-selection.component.ts
+++ src/app/components/home/shared/hl-quick-selection/hl-quick-selection.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { IonModal } from '@ionic/angular/common';
 import { IonContent } from "@ionic/angular/standalone";
+import { HlQuantityComponent } from '../hl-quantity/hl-quantity.component';
 
 @Component({
   selector: 'app-hl-quick-selection',
   templateUrl: './hl-quick-selection.component.html',
   styleUrls: ['./hl-quick-selection.component.scss'],
   standalone: true,
-  imports: [IonicModule, ]
+  imports: [IonicModule,HlQuantityComponent ]
 })
 export class HlQuickSelectionComponent    {
 
